eb661c7653d1ca99a4515b15c22c55d525131cfb
Rodrigo Castro <zavalik@live.com>
2025-03-21 20:40:14 -0300

    Corrigindo caminho do arquivo e solucao.

--- start.sh
+++
@@ -1,9 +0,0 @@
-# ECR Login
-sudo aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 588738580149.dkr.ecr.us-east-1.amazonaws.com
-
-# Stack Deploy
-sudo docker stack deploy -c docker-compose.yml jokestack
-
-# Stack Status
-sudo docker stack ps jokestack
-sudo docker stack services jokestack
